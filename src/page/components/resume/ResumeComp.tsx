8508ec28a357ab291732d7feeaa90097db682424
JuniFruit@github.com <idiot228322@mail.ru>
2023-02-23 00:03:57 +0300

    CV link changed

--- src/page/components/resume/ResumeComp.tsx
+++ src/page/components/resume/ResumeComp.tsx
@@ -1,22 +1,18 @@
-import { FC } from 'react'
-import { Button } from '../../ui/buttons/button/Button';
-import { ButtonLink } from '../../ui/buttons/link/ButtonLink';
-import HeadingSection from '../../ui/headers/HeadingSection';
-import styles from './ResumeComp.module.scss';
+import { FC } from "react";
+import { Button } from "../../ui/buttons/button/Button";
+import { ButtonLink } from "../../ui/buttons/link/ButtonLink";
+import HeadingSection from "../../ui/headers/HeadingSection";
+import styles from "./ResumeComp.module.scss";
 
 const ResumeComp: FC = () => {
-    return (
-        <div className={styles.wrapper}>
-            <HeadingSection>
-                You can download my CV right away
-            </HeadingSection>
-            <ButtonLink
-                href='https://drive.google.com/uc?export=download&id=1j3BQ3m45Qg-Kn4FzGdptcevv-WK_Cs5Y'
-            >
-                Download
-            </ButtonLink>
-        </div>
-    )
-}
+  return (
+    <div className={styles.wrapper}>
+      <HeadingSection>You can download my CV right away</HeadingSection>
+      <ButtonLink href="https://drive.google.com/uc?export=download&id=1BMdvyH7h_vxl93tPxuwI_JyuT0qyy8l3">
+        Download
+      </ButtonLink>
+    </div>
+  );
+};
 
-export default ResumeComp
+export default ResumeComp;
